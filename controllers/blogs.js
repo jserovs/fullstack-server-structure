8e87e7b3c84b4f9c053ac26a0a0e6b691d985b60
Jevgenijs Serovs <jevgenijs.serovs@gmail.com>
2021-01-10 22:38:50 +0200

    4.20 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog
     .find({}).populate('user')
@@ -19,12 +11,11 @@ blogsRouter.get('/', async (request, response, next) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const token = getTokenFrom(request)
-  const decodedToken = await jwt.verify(token, process.env.SECRET, (error, result) => {
+  const decodedToken = await jwt.verify(request.token, process.env.SECRET, (error, result) => {
     next(error)
   })
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
   const user = await User.findById(decodedToken.id)
